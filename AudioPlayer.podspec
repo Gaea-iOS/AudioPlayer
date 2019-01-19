d6557d6d155772ac7319393e62f86e6b5825623b
jerry <445242970@qq.com>
2019-01-19 15:32:58 +0800

    fix bugs

--- AudioPlayer.podspec
+++ AudioPlayer.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'AudioPlayer'
-  s.version          = '0.5.4'
+  s.version          = '0.5.6'
   s.summary          = 'A short description of AudioPlayer.'
 
 # This description is used to generate tags and improve search results.
--- AudioPlayer/Classes/player/AudioPlayer.swift
+++ AudioPlayer/Classes/player/AudioPlayer.swift
@@ -71,9 +71,6 @@ public class AudioPlayer: NSObject {
     /// The current item being played.
     public internal(set) var currentItem: AudioItem? {
         didSet {
-            if let previousItem = oldValue {
-                delegate?.audioPlayer(self, didEndPlaying: previousItem, error: nil)
-            }
             if let currentItem = currentItem {
                 //Stops the current player
                 player?.rate = 0
